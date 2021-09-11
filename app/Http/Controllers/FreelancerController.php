375c1c1fdb6f3b40677c6f14df011eb61931b78c
AR Uddin Shovon <69668471+ICaaaArush@users.noreply.github.com>
2021-09-11 09:57:13 +0800

    merge

--- app/Http/Controllers/FreelancerController.php
+++ app/Http/Controllers/FreelancerController.php
@@ -47,31 +47,10 @@ class FreelancerController extends Controller
 
                     //  SHOW DASHBOARD IF SUBSCRIBED
 
-
                     $products = ProductDetail::all();
 
                     return view('user.customer-dashboard',compact('products'));
-
-                    if(Auth::user()->status == 1){
-                        $products = ProductDetail::all();
-                        $data = QuickStart::all();
-
-                        $invoices = Auth()->user()->invoices();
-                        dd($invoices);
-
-                    return view('user.customer-dashboard',compact('products','data'));
-
-                    }else{
-
                     }
-                    Auth::logout();
-
-                    $request->session()->invalidate();
-            
-                    $request->session()->regenerateToken();
-            
-                    return view('auth.login');
-
 
             }elseif (Auth::user()->user_type == "freelancer") {
 
