0922e3ababe1536ffceb876574f15b07553d1645
AR Uddin Shovon <69668471+ICaaaArush@users.noreply.github.com>
2021-09-19 04:11:33 +0800

    pull

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -26,6 +26,7 @@ class FileUploadController extends Controller
             $fileName = new FileName;
             $fileName->video_name = $videoFileName;
             $fileName->save();
+            return $fileName->video_name;
         }
 
         //  GIF UPLOAD METHOD START
